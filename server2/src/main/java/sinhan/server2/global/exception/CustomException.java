9169e4fb7b905e65e4742cbcbfbb15d518322fd6
allllfo <mirae4292@gmail.com>
2024-06-10 09:33:31 +0900

    refactor : global 폴더 생성 후 ApiUtils, CustomException 생성

---
+++ server2/src/main/java/sinhan/server2/global/exception/CustomException.java
@@ -0,0 +1,12 @@
+package sinhan.server2.global.exception;
+
+import lombok.Getter;
+
+@Getter
+public class CustomException extends RuntimeException {
+    private final ErrorCode errorCode;
+    public CustomException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+}
