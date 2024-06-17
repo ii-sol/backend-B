ac06f441cd35693f6c272125ad328948aab80ae8
allllfo <mirae4292@gmail.com>
2024-06-17 11:31:10 +0900

    feat : AccountUtils( 받은 계좌객체로 송금, 계좌번호로 계좌 조회, 계좌 잔액 update ), UserUtils 생성( serialNumber로 사용자 조회 )

---
+++ server2/server-common/src/main/java/sinhan/server1/global/utils/user/UserUtils.java
@@ -0,0 +1,25 @@
+package sinhan.server1.global.utils.user;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import sinhan.server1.domain.tempuser.TempUser;
+import sinhan.server1.domain.tempuser.TempUserRepository;
+import sinhan.server1.global.exception.CustomException;
+import sinhan.server1.global.exception.ErrorCode;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+@Transactional
+public class UserUtils {
+
+    private final TempUserRepository tempUserRepository;
+
+    //serailNumber로 사용자 조회
+    public TempUser getUser(long serialNumber){
+        return tempUserRepository.findBySerialNumber(serialNumber)
+                .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_USER));
+    }
+}
