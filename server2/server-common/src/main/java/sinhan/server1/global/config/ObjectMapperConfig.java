50028a500136fff4c9de4612eed22c0273dd9efe
ye-s-rin <pyr53540@gmail.com>
2024-06-19 16:25:20 +0900

    feat: LOWER_CAMEL_CASE, SimpleDateFormat

--- server2/server-common/src/main/java/sinhan/server1/global/config/ObjectMapperConfig.java
+++ server2/server-common/src/main/java/sinhan/server1/global/config/ObjectMapperConfig.java
@@ -1,14 +1,21 @@
 package sinhan.server1.global.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.text.SimpleDateFormat;
 
 @Configuration
 public class ObjectMapperConfig {
 
     @Bean
     public ObjectMapper objectMapper() {
-        return new ObjectMapper();
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setPropertyNamingStrategy(PropertyNamingStrategy.LOWER_CAMEL_CASE);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        objectMapper.setDateFormat(dateFormat);
+        return objectMapper;
     }
 }
